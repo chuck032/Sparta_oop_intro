c92c1e41764a26b8b9c9f36a3014157107a209a5
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2018-06-05 14:02:34 +0100

    finished living organism super classes

--- oop_starter_code/living_organism.rb
+++ oop_starter_code/living_organism.rb
@@ -1,7 +1,23 @@
 # This is the super class of defining a living organism
 class LivingOrganism
 
+  # def alive
+    # instance method
+  #   puts "Living organisms move, feed, respire, grow, sense their environment, excrete and reproduce. Can be summarizesd using MRS GREN."
+  # end
+  #
+  # cell = LivingOrganism.new
+  # cell.alive
+
+  def self.alive
+    # Class method
+    puts "Living organisms move, feed, respire, grow, sense their environment, excrete and reproduce. Can be summarizesd using MRS GREN."
+
+    $global_variable = "I am available everywhere"
+
+  end
+
+  # LivingOrganism.alive
 
 
 end
-
--- oop_starter_code/mammals/dog.rb
+++ oop_starter_code/mammals/dog.rb
@@ -1,9 +1,28 @@
 require_relative '../animal'
 require_relative '../animal_types/mammals'
 # Our dog class/template will inherit from animal and be affected by animal types
-class Dog
+class Dog < Animal
+  include Mammals #need this to access class inside module mammals.rb
 
+  def speak
+    puts "woof"
+  end
+
+  def number_of_legs
+    Biped.new
+  end
+
+  def super_duper(arg1, arg2)
+    puts "This is my super_duper instance methos which we inherited from animal"
+    # call instance method of same name in superclass
+    puts "Args in inherited class #{arg1 ? arg1 : "noarg1"} #{arg2 ? arg2 : "noarg2"}"
+    # with args
+    super
+
+  end
 
 end
 
-
+fido = Dog.new
+fido.speak #should get woof
+fido.number_of_legs
